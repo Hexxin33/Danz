5eeb17170db66f828bab95b0d5654818d2a0830c
Hexxin33 <112873089+Hexxin33@users.noreply.github.com>
2023-03-04 21:43:47 +0700

    Update textMenu.js

--- setting/textMenu.js
+++ setting/textMenu.js
@@ -1,9 +1,17 @@
 exports.textMenu = (sender, nama, namabot) => {
   return `*Halo @${sender.split("@")[0]}* 👋
 
-[ *RANDOM-MENU* ]
-• !sticker
-• !info
+[ *TOOLS-MENU* ]
+• #owner
+• #sticker
+• #info
+
+[ *GRUP-MENU* ]
+• #add [628××××××××××]
+• #kick [@tag]
+• #promote [@tag]
+• #demote [@tag]
+• #welcome [On/Off]
 
 © ${namabot} | by *${nama}* | 🇮🇩`
 }
