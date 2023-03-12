6b53eb95c8e680cba0109948be1895c98ac0f01f
Hexxin33 <112873089+Hexxin33@users.noreply.github.com>
2023-03-12 09:24:08 +0700

    Update textMenu.js

--- setting/textMenu.js
+++ setting/textMenu.js
@@ -8,19 +8,42 @@ exports.textMenu = (sender, nama, namabot) => {
 
 [ *GRUP-MENU* ]
 • #add [628××××××××××]
+• #hidetag
 • #kick [@tag]
 • #promote [@tag]
 • #demote [@tag]
 • #welcome [On/Off]
+• #bukagroup
+• #tutupgroup
+• #setnama
 
 [ *RANDOM-MENU*]
-• #menfess [628××××××××××]
+• #menfess [628××××××××××&text]
 • #simi [text]
 
 [ *ANIME-MENU* ]
 • #kiss
 • #neko
 • #waifu
+• #hneko [NFSW]
+• #hwaifu [NFSW]
+
+[ *AUDIO-MENU* ]
+• #audio1
+• #audio2
+• #audio3
+• #audio4
+• #audio5
+• #audio6
+• #audio7
+• #audio8
+• #audio8
+• #audio10
+• #audio11
+• #audio12
+• #audio13
+• #audio14
+• #audio15
 
 © ${namabot} | by *${nama}* | 🇮🇩`
 }
