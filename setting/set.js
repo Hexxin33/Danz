e56b7bae98367aca45a880527d823afca85fcfa5
Hexxin33 <112873089+Hexxin33@users.noreply.github.com>
2023-03-13 19:54:27 +0700

    Update set.js

--- setting/set.js
+++ setting/set.js
@@ -1,6 +1,6 @@
 // Silahkan ubah di bawah ini semau kalian
 var nama = 'Zizz'
-var namabot = 'DanzBot'
+var namabot = 'ᴅᴀɴᴢʙᴏᴛᴢ'
 var owner = '6281380979628'
 var reply = '[ BOT ] WHATSAPP'
 var donasi = 'undefined'
